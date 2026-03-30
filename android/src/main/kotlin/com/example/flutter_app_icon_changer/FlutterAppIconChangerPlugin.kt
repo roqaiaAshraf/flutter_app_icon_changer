029bd7f626f36b263850c4f1dec09b00c7b1ed46
roqaia ashraf <rokaya.ashraf49@gmail.com>
2026-03-30 17:00:36 +0200

    update

--- android/src/main/kotlin/com/example/flutter_app_icon_changer/FlutterAppIconChangerPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_app_icon_changer/FlutterAppIconChangerPlugin.kt
@@ -96,13 +96,13 @@ val iconToChange = iconName?.let { name ->
 
 
         enableComponent(pm, packageName, icon)
-        Handler(Looper.getMainLooper()).postDelayed({
-            relaunchApp()
-        }, 1000)        
+      
         availableIcons.filter { it.icon != icon }.forEach {
         
         }
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            relaunchApp()
+        }, 1000)  
     }
 private fun relaunchApp() {
     val context = binding.applicationContext
